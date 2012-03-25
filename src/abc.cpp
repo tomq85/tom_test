324b5a793ee323d415f018346e0086828783cd9a
Tomasz <tomq85@gmail.com>
2012-03-25 21:45:15 +0200

    changes

--- src/abc.cpp
+++ src/abc.cpp
@@ -11,7 +11,7 @@
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!aaHello World!!!" << endl; // prints !!!Hello World!!!
 	TestClass().helloTest();
 	return 0;
 }
